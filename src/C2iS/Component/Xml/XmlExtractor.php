43c063a433063b6d370379cbfe036556fd9634a1
Guillaume Manen <g.manen@c2is.fr>
2015-07-21 15:17:27 +0200

    Refactoring

--- src/C2iS/Component/Xml/XmlExtractor.php
+++ src/C2iS/Component/Xml/XmlExtractor.php
@@ -29,7 +29,9 @@ class XmlExtractor
             $dom = $input;
         } else {
             $dom = new \DOMDocument();
+            set_error_handler(function() { /* ignore warning errors from DomDocument::loadXML*/ }, E_WARNING);
             @$dom->loadXML($input);
+            restore_error_handler();
         }
 
         $domXpath = new \DOMXPath($dom);
